a38d35dade839a4f913f2875bceb11ab867eb6d8
josephrobinsonsimon@gmail.com <josephrobinsonsimon@gmail.com>
2024-07-10 17:15:16 +0100

    Added .env

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -4,9 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 import { Readable } from "stream";
 
-
-const resend = new Resend('re_123456789');
-resend.apiKeys.create({ name: 'Production' });
+const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const email = formData.get("email") as string;
